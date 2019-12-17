f2ff63d30d9c5fd369095572bc13db38a086f511
jdfrozen <616769416@qq.com>
2019-12-17 13:43:04 +0800

    增加server

---
+++ spring-cloud-server/src/main/java/com/frozen/springcloudserver/SpringCloudServerApplication.java
@@ -0,0 +1,15 @@
+package com.frozen.springcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerApplication.class, args);
+	}
+
+}
