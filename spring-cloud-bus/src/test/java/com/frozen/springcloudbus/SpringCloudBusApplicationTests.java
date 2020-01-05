6c8d52bce44d9f99a21396dc990983d49729b7bf
jdfrozen <616769416@qq.com>
2020-01-05 17:41:21 +0800

    添加bus

---
+++ spring-cloud-bus/src/test/java/com/frozen/springcloudbus/SpringCloudBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frozen.springcloudbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
