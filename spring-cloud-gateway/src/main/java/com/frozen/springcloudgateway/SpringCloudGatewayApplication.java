6042a45699cb0a6371469751b3821e272a3e049c
jdfrozen <616769416@qq.com>
2020-01-10 14:08:07 +0800

    Gateway

--- spring-cloud-gateway/src/main/java/com/frozen/springcloudgateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/frozen/springcloudgateway/SpringCloudGatewayApplication.java
@@ -2,6 +2,12 @@ package com.frozen.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -10,5 +16,41 @@ public class SpringCloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudGatewayApplication.class, args);
 	}
+//	@Bean
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route(p -> p
+//						.path("/get")
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+//						.uri("http://httpbin.org:80"))
+//				.build();
+//	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		String httpUri = "http://httpbin.org:80";
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri(httpUri))
+				.route(p -> p
+						.host("*.hystrix.com")
+						.filters(f -> f
+								.hystrix(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
+						.uri(httpUri))
+				.build();
+	}
+
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+
+// curl --dump-header - --header 'Host: www.hystrix.com' http://localhost:8080/delay/3
+
 
 }
