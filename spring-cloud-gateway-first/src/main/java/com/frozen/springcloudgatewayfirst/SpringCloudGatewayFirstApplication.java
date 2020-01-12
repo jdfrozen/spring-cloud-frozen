d2aa2929caf4c406ecad7968ad1115afd604e5e2
jdfrozen <616769416@qq.com>
2020-01-12 19:26:01 +0800

    add module

---
+++ spring-cloud-gateway-first/src/main/java/com/frozen/springcloudgatewayfirst/SpringCloudGatewayFirstApplication.java
@@ -0,0 +1,13 @@
+package com.frozen.springcloudgatewayfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayFirstApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-gateway-first/src/test/java/com/frozen/springcloudgatewayfirst/SpringCloudGatewayFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frozen.springcloudgatewayfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
