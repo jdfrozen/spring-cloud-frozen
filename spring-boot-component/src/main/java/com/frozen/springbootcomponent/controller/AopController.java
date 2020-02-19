4c646fcbb1a6eeee69ef0ceff1c8a84c38555385
jdfrozen <616769416@qq.com>
2020-02-19 18:34:28 +0800

    add aop

---
+++ spring-boot-component/src/main/java/com/frozen/springbootcomponent/controller/AopController.java
@@ -0,0 +1,23 @@
+package com.frozen.springbootcomponent.controller;
+
+import com.frozen.springbootcomponent.annocation.AopAnnocation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 健康检查
+ */
+@RestController
+public class
+AopController {
+
+    @RequestMapping("/info")
+    @AopAnnocation(name = "测试方法", desc = "测试方法")
+    public String info(String tt){return "success";}
+
+    @RequestMapping("/faild")
+    @AopAnnocation(name = "测试异常方法", desc = "方法描述")
+    public void error(String tt){
+        throw new RuntimeException("测试异常方法Runtime");
+    }
+}
